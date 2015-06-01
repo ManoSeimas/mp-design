29a214b1fff65e292495d14d4d280fe513938d50
Jaro <jaro@nous.lt>
2015-06-02 00:16:44 +0300

    Statistics blocks styles added.

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('.ui.checkbox').checkbox('toggle');
   $('.ui.dropdown').dropdown();
+  $('.ui.indicating.progress').progress({showActivity: false});
   // $('.ui.sticky').sticky({context: '#example1'});
 });
